87155a1cbce406dbc51da4800f4cdff9aa51693b
irailtonreis <irailton.oliveira91@gmail.com>
2020-10-08 23:36:55 -0300

    feat: adiciona components inicias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import { BrowserRouter, Routes,  Route  } from 'react-reouter-dom'
 
+import {  } from 'reac';
 function App() {
   return (
     <div className="App">
-    insta dogs
+      <BrowserRouter>
+        
+        <Header />
+          <Routes>
+            <Route  path="/" element={<Home />}/>
+            <Route  />sss
+          </Routes>
+        <Footer />
+      </BrowserRouter>s
     </div>
   );
 }
