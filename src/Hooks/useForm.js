f22807f8a817e8459f7bf39a7a2bb65209e7fdd6
irailtonreis <irailton.oliveira91@gmail.com>
2020-10-24 10:53:20 -0200

    feat: adiciona preview e postagem de foto

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -8,6 +8,10 @@ const types ={
     password: {
         regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
         message: "A senha precisa ter 1 caracter maiúsculo, 1 minúculo e um dígito. Com no mínimo 8 caracteres."
+    },
+    number: {
+        regex: /^\d+$/,
+        message: "Utilize apenas número."
     }
 }
 const useForm = (type) => {
