037cf6e8e661f7e7651824ac29e692b0e03fe5d4
irailtonreis <irailton.oliveira91@gmail.com>
2020-10-23 23:56:51 -0200

    feat: adiciona form post foto

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import styles from './UserPhotoPost.module.css';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
 
 const UserPhotoPost = () => {
+    const nome = useForm()
+    const peso = useForm('number')
+    const idade = useForm('number')
+    const [img, setImg] = React.useState({})
+    const { data, error, loading, request } = useFetch()
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData()
+        formData.append('img', img.value)
+        formData.append('nome', nome.value)
+        formData.append('idade', idade.value)
+    }
+
+    function handleImageChange({target}) {
+        setImg({
+            raw: target.files[0]
+        })
+        event.preventDefault();
+
+        
+    }
+    
     return (
-        <div>
-            foto post
-        </div>
+        <section className={`${styles.photoPost} animeLeft`}>
+            <form onSubmit={handleSubmit}>
+                <Input  label="Nome" type="text" name="nome" />
+                <Input  label="Peso" type="text" name="Peso" />
+                <Input  label="Idade" type="text" name="Idade" />
+                <input type="file" name="img" id="img" onChange={handleImageChange} />
+                <Button>Enviar</Button> 
+
+
+            </form>
+        </section>
     )
 }
 
